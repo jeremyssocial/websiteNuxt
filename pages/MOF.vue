ebb5be625153f82c59ad52ac931648507ab2e15f
jeremyssocial <original.jeremy.grotjahn@gmail.com>
2023-08-30 19:30:58 +0200

    added MOF.vue with meta tags

---
+++ pages/MOF.vue
@@ -0,0 +1,44 @@
+<template>ok</template>
+
+<script>
+export default {
+  head: {
+    title: "MOF - The New Movement in German Rap Scene | @nr.bln",
+    meta: [
+      {
+        name: "description",
+        content: "Discover the new movement in the German rap scene, MOF.",
+      },
+      {
+        name: "keywords",
+        content: "MOF, German Rap, Music, Discussion, nr.bln, nrbln",
+      },
+      {
+        property: "og:title",
+        content: "MOF - The New Movement in German Rap Scene | @nr.bln",
+      },
+      {
+        property: "og:description",
+        content: "Discover the new movement in the German rap scene, MOF.",
+      },
+      {
+        property: "og:url",
+        content: "https://near-berlin.de/MOF",
+      },
+      {
+        property: "og:image",
+        content:
+          "https://preview.redd.it/20inaqkugvkb1.jpg?width=1440&format=pjpg&auto=webp&s=4cb5928824f9b8306311b51fe85c6ecab0515eb2",
+      },
+      {
+        property: "og:type",
+        content: "website",
+      },
+      {
+        property: "og:site_name",
+        content: "nr.bln",
+      },
+    ],
+  },
+};
+</script>
