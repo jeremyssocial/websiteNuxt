c0e5ddf9b8e104a3fa744838928eaded37928472
jeremyssocial <original.jeremy.grotjahn@gmail.com>
2023-08-31 19:08:36 +0200

    added glitch text "nr.bln" to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="cube-container">
-    <div class="cube">
-      <div class="face front">nr.bln</div>
-      <div class="face back">near berlin</div>
-      <div class="face right">fashion</div>
-      <div class="face left">photography</div>
-      <div class="face top">development</div>
-      <div class="face bottom">social media</div>
+  <div id="index-content">
+    <GlitchText text="nr.bln" />
+    <div class="cube-container">
+      <div class="cube">
+        <div class="face front">nr.bln</div>
+        <div class="face back">near berlin</div>
+        <div class="face right">fashion</div>
+        <div class="face left">photography</div>
+        <div class="face top">development</div>
+        <div class="face bottom">social media</div>
+      </div>
     </div>
   </div>
 </template>
