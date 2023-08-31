235fda926ea9d2f95e666443128d9c21b21b2b93
jeremyssocial <original.jeremy.grotjahn@gmail.com>
2023-08-31 07:09:30 +0200

    added header, footer and global css file

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>© 2023 nr.bln. Alle Rechte vorbehalten.</p>
+      <p>
+        <nuxt-link to="/impressum">Impressum</nuxt-link>
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <nuxt-link to="/">
+      <img src="~/static/logo.png" alt="Logo" class="logo" />
+    </nuxt-link>
+    <nav class="nav">
+      <ul>
+        <li>
+          <nuxt-link to="/impressum">Impressum</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
