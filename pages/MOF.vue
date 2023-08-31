b26c1a2ce4d36e38255150f196643668c0afb63d
jeremyssocial <original.jeremy.grotjahn@gmail.com>
2023-08-31 06:18:27 +0200

    more SEO for MOF.vue but in german

--- pages/MOF.vue
+++ pages/MOF.vue
@@ -3,42 +3,77 @@
 <script>
 export default {
   head: {
-    title: "MOF - The New Movement in German Rap Scene | @nr.bln",
+    title: "MOF - Entdecke die neue Deutschrap-Bewegung | @nr.bln",
     meta: [
       {
+        hid: "description",
         name: "description",
-        content: "Discover the new movement in the German rap scene, MOF.",
+        content:
+          "Dive in das Herz der MOF-Bewegung, eine neue Außenseiterbewegung in der deutschen Rap-Szene. Entdecke die tiefgehenden Lyrics, das einzigartige Handzeichen und die starke Gemeinschaft, die MOF definiert.",
       },
       {
+        hid: "keywords",
         name: "keywords",
-        content: "MOF, German Rap, Music, Discussion, nr.bln, nrbln",
+        content:
+          "MOF, Mensch Ohne Freunde, Deutschrap, Ufo361, Außenseiterkultur, Rap-Musik, Musikdiskussion, Berliner Rapper, nr.bln",
       },
       {
+        hid: "og:title",
         property: "og:title",
-        content: "MOF - The New Movement in German Rap Scene | @nr.bln",
+        content: "MOF - Entdecke die neue Deutschrap-Bewegung | @nr.bln",
       },
       {
+        hid: "og:description",
         property: "og:description",
-        content: "Discover the new movement in the German rap scene, MOF.",
+        content:
+          "Tauche ein in das Herz der MOF-Bewegung, eine revolutionäre neue Außenseiterkultur in der deutschen Rap-Szene mit Ufo361 an der Spitze.",
       },
       {
+        hid: "og:url",
         property: "og:url",
         content: "https://near-berlin.de/MOF",
       },
       {
+        hid: "og:image",
         property: "og:image",
         content:
           "https://preview.redd.it/20inaqkugvkb1.jpg?width=1440&format=pjpg&auto=webp&s=4cb5928824f9b8306311b51fe85c6ecab0515eb2",
       },
       {
+        hid: "og:type",
         property: "og:type",
         content: "website",
       },
       {
+        hid: "og:site_name",
         property: "og:site_name",
         content: "nr.bln",
       },
     ],
+    structuredData: {
+      "@context": "https://schema.org",
+      "@type": "Article",
+      headline: "MOF - Entdecke die neue Deutschrap-Bewegung",
+      image: [
+        "https://preview.redd.it/20inaqkugvkb1.jpg?width=1440&format=pjpg&auto=webp&s=4cb5928824f9b8306311b51fe85c6ecab0515eb2",
+      ],
+      datePublished: "2022-01-01",
+      dateModified: "2022-02-01",
+      author: {
+        "@type": "Person",
+        name: "nr.bln",
+      },
+      publisher: {
+        "@type": "Organization",
+        name: "nr.bln",
+        logo: {
+          "@type": "ImageObject",
+          url: "https://near-berlin.de/logo.png",
+        },
+      },
+      description:
+        "Tauche ein in das Herz der MOF-Bewegung, eine revolutionäre neue Außenseiterkultur in der deutschen Rap-Szene mit Ufo361 an der Spitze.",
+    },
   },
 };
 </script>
