5659a7e0e07b40e8bbd544dba714517b3a3c23b6
jeremyssocial <original.jeremy.grotjahn@gmail.com>
2023-08-31 09:55:45 +0200

    added content to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,22 @@
-<template></template>
+<template>
+  <div class="cube-container">
+    <div class="cube">
+      <div class="face front">nr.bln</div>
+      <div class="face back">near berlin</div>
+      <div class="face right">nr.bln</div>
+      <div class="face left">near berlin</div>
+      <div class="face top">nr.bln</div>
+      <div class="face bottom">near berlin</div>
+    </div>
+  </div>
+</template>
 
+<style>
+@import "~/assets/style/index/index.css";
+</style>
 <script>
 export default {
   name: "IndexPage",
+  head: {},
 };
 </script>
