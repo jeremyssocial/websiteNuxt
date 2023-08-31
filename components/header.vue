6bb716dff7d77b01a80a29866c0ad21a9e0a1b67
jeremyssocial <original.jeremy.grotjahn@gmail.com>
2023-08-31 17:31:09 +0200

    put header css in header component

--- components/header.vue
+++ components/header.vue
@@ -1,2 +1,67 @@
+<template>
+  <header class="header">
+    <nuxt-link to="/">
+      <img src="~/static/logo.png" alt="Logo" class="logo" />
+    </nuxt-link>
+    <nav class="nav">
+      <ul>
+        <li>
           <InstagramLogoLink />
-          <InstagramLogoLink />
+        </li>
+        <li>
+          <nuxt-link to="/impressum">Impressum</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  margin-bottom: 20px;
+}
+
+.logo {
+  height: 50px;
+}
+
+.nav ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+}
+
+.nav ul li {
+  margin-left: 25px;
+  position: relative;
+}
+
+.nav ul li::after {
+  content: "|";
+  position: absolute;
+  right: -15px;
+  top: 0;
+}
+
+.nav ul li:last-child::after {
+  content: "";
+}
+
+/* Hover effects */
+
+.nav ul li a:hover,
+.content a:hover,
+a:hover {
+  color: #3700b3;
+}
+</style>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
