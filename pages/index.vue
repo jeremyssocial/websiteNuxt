2ccb6689437dee5f1cbe790719cfce36b333ac31
jeremyssocial <original.jeremy.grotjahn@gmail.com>
2023-08-31 18:16:41 +0200

    index SEO

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <div class="face back">near berlin</div>
       <div class="face right">fashion</div>
       <div class="face left">photography</div>
-      <div class="face top">developement</div>
+      <div class="face top">development</div>
       <div class="face bottom">social media</div>
     </div>
   </div>
@@ -17,6 +17,48 @@
 <script>
 export default {
   name: "IndexPage",
-  head: {},
+  head: {
+    title: "nr.bln - Captivating Berlin Fashion & Street Photography",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        itemprop: "description",
+        content:
+          "Dive into the dynamic world of Berlin fashion and street photography with Jeremy. Discover the city through his lens, as featured on the @nr.bln Instagram account.",
+      },
+      {
+        hid: "og:title",
+        name: "og:title",
+        content: "nr.bln - Captivating Berlin Fashion & Street Photography",
+      },
+      { name: "og:type", content: "website" },
+      { name: "og:url", content: "https://near-berlin.de/" },
+      {
+        name: "og:image",
+        content:
+          "https://preview.redd.it/lmhgzfx1bvkb1.jpg?width=1500&format=pjpg&auto=webp&s=884f7c0dc9f94c8ab31d86a2a16db0e1876ffb69",
+      },
+      {
+        hid: "og:description",
+        name: "og:description",
+        content:
+          "Dive into the dynamic world of Berlin fashion and street photography with Jeremy. Discover the city through his lens, as featured on the @nr.bln Instagram account.",
+      },
+      { name: "og:locale", content: "en_US" },
+      {
+        name: "og:site_name",
+        content: "nr.bln - Captivating Berlin Fashion & Street Photography",
+      },
+      {
+        name: "keywords",
+        content:
+          "Berlin, Fashion, Street Photography, Urban Style, Jeremy, Instagram, nr.bln, near berlin, Berlin Style",
+      },
+      { name: "robots", content: "index, follow" },
+    ],
+  },
 };
 </script>
