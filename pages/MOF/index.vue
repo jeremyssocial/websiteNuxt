6ec0893765c7378cef0b94d89cc9d5321cddf0c1
jeremyssocial <original.jeremy.grotjahn@gmail.com>
2023-09-09 16:48:51 +0200

    fixed MOF index page

--- pages/MOF/index.vue
+++ pages/MOF/index.vue
@@ -1,3 +1,176 @@
+<template>
+  <div class="max-w-3xl mx-auto px-4">
+    <h1 class="text-3xl font-bold mb-4">
+      Willkommen bei <GlitchText text="MOF" /> - Die neue Bewegung in der
+      Deutschrap-Szene
+    </h1>
+
+    <p class="text-lg mb-4">
+      MOF ist eine aufstrebende Community innerhalb der Deutschrap-Szene. MOF,
+      die Abkürzung für "Mensch Ohne Freunde", repräsentiert eine Gruppierung
+      von Individualisten, die sich als Außenseiter verstehen und in Musik ihr
+      gemeinsames Heim finden. Hier dreht sich alles nicht ausschließlich um
+      einen Künstler oder Rapper, sondern um eine untereinander stützende
+      Gemeinschaft.
+    </p>
+
+    <h2 class="text-2xl font-semibold mb-2">Unsere Mission</h2>
+
+    <p class="text-lg mb-4">
+      Unser Ziel bei MOF ist es, eine unterstützende Community für Außenseiter
+      zu entwickeln, die sich innerhalb der konventionellen Strukturen und
+      Erwartungen der Gesellschaft weniger repräsentiert fühlen. Wir sind eine
+      Gemeinschaft, die sich an den Prinzipien der Akzeptanz, des Respekts und
+      der Musikalität orientiert.
+    </p>
+
+    <h2 class="text-2xl font-semibold mb-2">Wovon wir uns distanzieren</h2>
+
+    <p class="text-lg mb-4">
+      Bei MOF setzen wir uns für eine inklusive, respektvolle und vielfältige
+      Gemeinschaft ein. Deshalb distanzieren wir uns ausdrücklich von jeglicher
+      Form von Hass und Diskriminierung.
+    </p>
+
+    <details class="mb-4">
+      <summary class="cursor-pointer text-blue-500 hover:underline">
+        Dies umfasst, ist aber nicht beschränkt auf:
+      </summary>
+
+      <ul class="list-disc list-inside mt-2">
+        <li>Homophobie</li>
+        <li>Rassismus</li>
+        <li>Rechtsextremismus</li>
+        <li>Gewaltverherrlichung</li>
+        <li>Sexismus</li>
+        <li>Transphobie</li>
+        <li>
+          Ableismus (Diskriminierung aufgrund physischer oder mentaler
+          Fähigkeiten)
+        </li>
+        <li>Unterstützung von autokratischen Regimen oder Reichsbürgern</li>
+        <li>Gründung oder Unterstützung von Sekten</li>
+      </ul>
+    </details>
+
+    <p class="text-lg mb-4">
+      Wir sind eine Gemeinschaft, die die demokratischen Werte hochhält und den
+      Respekt für die Rechte und Freiheiten aller Einzelnen betont. Individuen,
+      die sich diesen Werten widersetzen und behaupten, sie seien Teil von MOF,
+      werden von uns nicht offiziell anerkannt. Während wir die Meinungsfreiheit
+      respektieren, distanzieren wir uns entschieden von diesen Aspekten und
+      erkennen diese Personen nicht als Teil unserer Gemeinschaft an. Wir
+      glauben an die Kraft der Musik, um Menschen zusammenzubringen, und wir
+      stehen für ein Umfeld, in dem sich jeder sicher und akzeptiert fühlt.
+      Unser Ziel ist es, eine positive und unterstützende Atmosphäre zu fördern,
+      die die Vielfalt und Individualität unserer Gemeinschaft respektiert.
+    </p>
+
+    <h2 class="text-2xl font-semibold mb-2">Häufig gestellte Fragen</h2>
+
+    <p class="text-lg mb-4">
+      <strong>Q: Was bedeutet MOF?</strong><br />A: MOF steht für "Mensch Ohne
+      Freunde". MOF repräsentiert eine Gemeinschaft, die sich als Außenseiter
+      versteht und über die Konformität der Gesellschaft hinaus blickt.
+    </p>
+
+    <p class="text-lg mb-4">
+      <strong>Q: Was ist das MOF-Handzeichen?</strong><br />A: Das spezielle
+      MOF-Handzeichen wurde auf
+      <a
+        href="https://www.instagram.com/p/CwSN4CRNN-L/?img_index=1"
+        class="text-blue-500 hover:underline"
+        >Instagram</a
+      >
+      vorgestellt. Daumen, Zeigefinger und Ringfinger werden so gebeugt, dass
+      sie die Handfläche berühren. Die anderen beiden Finger bleiben
+      ausgestreckt, was die Initialen M, O und F repräsentiert.
+    </p>
+
+    <p class="text-lg mb-4">
+      <strong>Q: Wie genau repräsentiert das Handzeichen "MOF"?</strong>
+      <br />
+      A: Das MOF Handzeichen wurde sorgfältig konstruiert, um das Akronym "MOF"
+      zu repräsentieren. Man krümmt Daumen, Zeige- und Ringfinger so, dass sie
+      die Handinnenfläche berühren, was als Symbol für das "M" dient. Der
+      Abstand zwischen den drei gekrümmten Fingern und den zwei ausgestreckten
+      Fingern symbolisiert das "O" für "Ohne". Dieser Zwischenraum repräsentiert
+      den leeren Raum oder das "Null"-Element, welches oft mit "Ohne" assoziiert
+      wird. Schließlich repräsentieren die beiden ausgestreckten Finger, der
+      Mittel- und der kleine Finger, das "F".
+    </p>
+    <h2 class="text-2xl font-semibold mb-2">Ressourcen</h2>
+
+    <div class="text-lg mb-4">
+      <p>
+        Für weitere Informationen über MOF, wirf einen Blick auf unsere
+        offiziellen community Seiten auf Instagram:
+      </p>
+      <div class="flex flex-wrap justify-around">
+        <PersonCard
+          name="mofgang4l"
+          role="Community Page"
+          igLink="https://www.instagram.com/mofgang4l/"
+        />
+        <PersonCard
+          name="mofgang2023"
+          role="Community Page"
+          igLink="https://www.instagram.com/mofgang2023/"
+        />
+      </div>
+    </div>
+
+    <h2 class="text-2xl font-semibold mb-2">Ansprechpartner</h2>
+
+    <p class="text-lg mb-4">
+      Diese Website entstand aus einem Instagram-Gruppenchat von
+      MOF-Mitgliedern. Die Admins der Gruppe sind die primären Ansprechpartner
+      für Fragen, Feedback oder Vorschläge:
+    </p>
+
+    <div class="flex flex-wrap justify-around">
+      <PersonCard
+        name="nr.bln"
+        role="MOF Admin"
+        igLink="https://www.instagram.com/nr.bln/"
+      />
+
+      <PersonCard
+        name="mofgang4l"
+        role="MOF Admin"
+        igLink="https://www.instagram.com/mofgang4l/"
+      />
+
+      <PersonCard
+        name="maro_mbb"
+        role="MOF Admin"
+        igLink="https://www.instagram.com/maro_mbb/"
+      />
+
+      <PersonCard
+        name="shbztjonas"
+        role="MOF Admin"
+        igLink="https://www.instagram.com/shbztjonas/"
+      />
+
+      <PersonCard
+        name="361erjamie"
+        role="MOF Admin"
+        igLink="https://www.instagram.com/361erjamie/"
+      />
+    </div>
+
+    <h2 class="text-2xl font-semibold mb-2">Mach mit</h2>
+
+    <p class="text-lg mb-4">
+      Wir sind stolz auf die starke Gemeinschaft, die wir bisher aufgebaut haben
+      und würden uns freuen, dich als Teil von MOF willkommen zu heißen! Lass
+      uns zusammen eine Bewegung schaffen, die das Mainstream-Denken
+      herausfordert und die Deutschrap-Szene revolutioniert.
+    </p>
+  </div>
+</template>
+
 <script>
 import PersonCard from "../../components/PersonCard.vue";
 
